d6b4aac24313a9cdfc2f21dc508f3685c7e9827c
zy <1661996964@qq.com>
2022-11-06 09:15:31 +0800

    setter注入对象

---
+++ src/main/java/com/zy/spring/ioc/entity/Child.java
@@ -0,0 +1,22 @@
+package com.zy.spring.ioc.entity;
+
+public class Child {
+    private String name;
+    private Apple apple;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Apple getApple() {
+        return apple;
+    }
+
+    public void setApple(Apple apple) {
+        this.apple = apple;
+    }
+}
